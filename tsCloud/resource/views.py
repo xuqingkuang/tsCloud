ce84edba01ca6abcb2b253a0f5d899a3419340ab
X.Q. Kuang <xuqingkuang@gmail.com>
2013-10-29 18:00:00 +0800

    Rewrite submit view as Class Based View

--- tsCloud/resource/views.py
+++ tsCloud/resource/views.py
@@ -34,34 +34,24 @@ def download(request, slug):
         raise Http404(err)
     return redirect(res.download(source = request.REQUEST.get('source')))
 
-def submit(request, category_slug, template_name='resource/submit.html'):
-    # Get the root category for base form
-    try:
-        category = models.Category.objects.get(slug = category_slug)
-    except models.Category.DoesNotExist, err:
-        raise Http404(err)
+class SubmitView(CreateView):
+    template_name='resource/submit.html'
+    model_class = models.Resource
 
-    form_class = getattr(forms, category.form_class_name, None)
-    if not form_class:
-        raise Http404('Specific category is not exist')
+    def get_initial(self):
+        return {
+            'category': self.kwargs['category_slug'],
+        }
 
-    form_initial_data = {
-        'category': category.slug
-    }
-    form = form_class(initial=form_initial_data)
-
-    # Do the POST works
-    if request.method == 'POST':
-        form = form_class(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            if not request.REQUEST.get('continue'):
-                return redirect(form.instance.get_absolute_url())
-
-    # Render the form
-    return render(request, template_name, {
-        'form': form,
-    })
+    def get_form_class(self):
+        # Get the root category for base form
+        try:
+            category = models.Category.objects.get(
+                slug = self.kwargs['category_slug']
+            )
+        except models.Category.DoesNotExist, err:
+            raise Http404(err)
+        return getattr(forms, category.form_class_name)
 
 class RecommendationListView(ListView):
     template_name = 'resource/recommendation_list.html'
